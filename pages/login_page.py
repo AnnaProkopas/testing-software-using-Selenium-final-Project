ec3ce989c7bb87b956265746ac2de250b8f26808
user <anna_prokopas@mail.ru>
2019-09-22 22:38:44 +1000

    Correct style code

--- pages/login_page.py
+++ pages/login_page.py
@@ -14,7 +14,12 @@ class LoginPage(BasePage):
         assert self.browser.current_url.find("login") >= 0, "Url is incorrect"
 
     def should_be_login_form(self):
-        assert self.browser.find_element(*LoginPageLocators.LOGIN_EMAIL_FORM) and self.browser.find_element(*LoginPageLocators.LOGIN_PASS_FORM), "Form of login don't exist"
+        assert self.browser.find_element(*LoginPageLocators.LOGIN_EMAIL_FORM) \
+            and self.browser.find_element(*LoginPageLocators.LOGIN_PASS_FORM), \
+            "Form of login don't exist"
     
     def should_be_register_form(self):
-        assert self.browser.find_element(*LoginPageLocators.REG_EMAIL_FORM) and self.browser.find_element(*LoginPageLocators.REG_PASS1_FORM) and self.browser.find_element(*LoginPageLocators.REG_PASS2_FORM), "Form of register don't exist"
+        assert self.browser.find_element(*LoginPageLocators.REG_EMAIL_FORM) \
+            and self.browser.find_element(*LoginPageLocators.REG_PASS1_FORM) \
+            and self.browser.find_element(*LoginPageLocators.REG_PASS2_FORM), \
+            "Form of register don't exist"
--- pages/main_page.py
+++ pages/main_page.py
@@ -7,6 +7,7 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         link.click()
-        
+
     def should_be_login_link(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), \
+            "Login link is not presented"
