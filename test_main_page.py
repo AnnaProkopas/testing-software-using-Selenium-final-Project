20d8b7956869cfc864d361eecaf416581a1e5e65
user <anna_prokopas@mail.ru>
2019-09-22 15:12:23 +1000

    Add test go to login page for guest from main page

---
+++ test_main_page.py
@@ -0,0 +1,9 @@
+link = "http://selenium1py.pythonanywhere.com/"
+
+def test_guest_can_go_to_login_page(browser): 
+   browser.get(link) 
+   go_to_login_page(browser) 
+
+def go_to_login_page(browser):
+    link = browser.find_element_by_css_selector("#login_link")
+    link.click()
