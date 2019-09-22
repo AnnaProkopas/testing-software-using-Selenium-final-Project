de0e79643ded1895b821bb1abc6cb9e6258de682
user <anna_prokopas@mail.ru>
2019-09-22 18:03:56 +1000

    Add locators
    Now all selectors contains in one separate file

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,6 @@
 from pages.main_page import MainPage
 
-link = "http://selenium1py.pythonanywhere.com"
+link = "http://selenium1py.pythonanywhere.com/"
 
 def test_add_to_cart(browser):
     page = MainPage(browser, url=link)
