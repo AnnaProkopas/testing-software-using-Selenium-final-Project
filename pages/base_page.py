327a6393fbaa8982f156161e4d7da37af7e558ce
user <anna_prokopas@mail.ru>
2019-09-28 23:56:33 +1000

    change abstract login page link to base_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,6 +4,8 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from .locators import BasePageLocators
+
 
 class BasePage():
     browser: webdriver
@@ -37,3 +39,10 @@ class BasePage():
 
     def open(self):
         self.browser.get(self.url)
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 import pytest
 from pages.product_page import ProductPage
+from pages.login_page import LoginPage
 import time
 """
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
@@ -18,25 +19,40 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.open()
     page.add_product_to_busket()
 """
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 @pytest.mark.xfail(reason="guest see success message after adding product to busket")
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, url=link)
     page.open()
-    print(" ---- >>> <<< ---- ")
     page.add_product_to_busket()
     page.should_not_be_success_message()
 
-@pytest.mark.xfail(reason="guest don't see success message without adding product to busket")
+#@pytest.mark.xfail(reason="guest don't see success message without adding product to busket")
 def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, url=link)
     page.open()
     page.should_not_be_success_message()
 
 @pytest.mark.xfail(reason="success message don't disappeared after adding product to busket")
 def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, url=link)
     page.open()
     page.add_product_to_busket()
     page.should_not_be_success_message_after_time()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
